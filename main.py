921bba44035d48f43af5e6dc52efd6663225ae48
Michael Primavera <pastasauce59@gmail.com>
2023-04-14 23:28:26 -0400

    Created labels for website name, email/username and password as well as created their corresponding entries. Created generate pword button and add button.

--- main.py
+++ main.py
@@ -12,7 +12,31 @@ window.config(padx=20, pady=20)
 canvas = Canvas(width=200, height=200)
 logo_img = PhotoImage(file='logo.png')
 canvas.create_image(100, 100, image=logo_img)
-canvas.pack()
+canvas.grid(column=1, row=0)
+
+website_label = Label(text='Website:')
+website_label.grid(column=0, row=1)
+
+website_entry = Entry(width=35)
+website_entry.grid(column=1, row=1, columnspan=2)
+
+email_username_label = Label(text='Email/Username:')
+email_username_label.grid(column=0, row=2)
+
+email_username_entry = Entry(width=35)
+email_username_entry.grid(column=1, row=2, columnspan=2)
+
+password_label = Label(text='Password:')
+password_label.grid(column=0, row=3)
+
+password_entry = Entry(width=18)
+password_entry.grid(column=1, row=3)
+
+generate_pword_button = Button(text='Generate Password')
+generate_pword_button.grid(column=2, row=3)
+
+add_button = Button(text='Add', width=36)
+add_button.grid(column=1, row=4, columnspan=2)
 
 
 window.mainloop()
